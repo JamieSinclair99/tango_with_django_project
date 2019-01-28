761f4ee6f19958fd96a21dfdcfffe3e199072e41
“Jamie <2325933s@student.gla.ac.uk>
2019-01-28 21:18:10 +0000

    Chapter 6 complete

--- rango/views.py
+++ rango/views.py
@@ -11,7 +11,9 @@ def index(request):
 	# Place the list in our context_dict dictionary
 	# that will be passed to the template engine.
 	category_list = Category.objects.order_by('-likes')[:5]
-	context_dict = {'categories': category_list}
+	page_list = Page.objects.order_by('-views')[:5]
+	context_dict = {'categories': category_list, 'pages' : page_list}
+	
 	
 	# Return a rendered response to send to the client.
 	# We make use of the shortcut function to make our lives easier. 
@@ -57,3 +59,7 @@ def show_category(request, category_name_slug):
     
     # Go render the response and return it to the client.
 	return render(request, 'rango/category.html', context_dict)
+	
+
+	
+	
